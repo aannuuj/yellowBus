07fc4dd90671d64dcb2ffffc19908cc1be99e8bc
aannuuj <hariompalkar@gmail.com>
2020-02-05 11:04:28 +0530

    initial commit

--- yellowBus/ViewController.swift
+++ yellowBus/ViewController.swift
@@ -9,9 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let segmentedControl : UISegmentedControl = {
+     let sc = UISegmentedControl(items: ["Live","Schedule"])
+        sc.selectedSegmentIndex = 0
+        return sc
+    }()
+    
+    let tableView = UITableView(frame: .zero, style: .plain)
+    let live = [
+    "Bus1","Bus2","Bus 2"]
+    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        navigationItem.title = "Shuttle Bus"
+        let stackView = UIStackView(arrangedSubviews: [
+        segmentedControl, tableView
+        ])
+        stackView.axis = .vertical
+        
+        view.addSubview(stackView)
+        stackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor,  padding: .zero)
+        
         // Do any additional setup after loading the view.
     }
 
